dcc36fa4e9638ddd7b369984b057d34e0748928a
ebruaeo <ebruaeo@hotmail.com>
2024-08-18 18:19:44 +0300

    viewmodel initialized and observing live data

--- app/src/main/java/com/example/foodbook/view/FoodDetailFragment.kt
+++ app/src/main/java/com/example/foodbook/view/FoodDetailFragment.kt
@@ -5,14 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.example.foodbook.databinding.FragmentFoodDetailBinding
 import com.example.foodbook.databinding.FragmentFoodListBinding
+import com.example.foodbook.util.downloadPics
+import com.example.foodbook.util.makePlaceHolder
+import com.example.foodbook.viewmodel.FoodDetailViewModel
 
 
 class FoodDetailFragment : Fragment() {
     private var _binding: FragmentFoodDetailBinding? = null
     private val binding get() = _binding!!
-
+    private lateinit var viewModel: FoodDetailViewModel
+    var foodId = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +34,26 @@ class FoodDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this)[FoodDetailViewModel::class.java]
+        arguments?.let {
+            foodId = FoodDetailFragmentArgs.fromBundle(it).foodId
+        }
+
+        viewModel.roomData(foodId)
+
+        observeLiveData()
+    }
+
+    private fun observeLiveData() {
+        viewModel.foodLiveData.observe(viewLifecycleOwner) {
+            binding.foodName.text = it.name
+            binding.foodCalorie.text = it.calorie
+            binding.foodProtein.text = it.protein
+            binding.foodCarbs.text = it.carbs
+            binding.foodFat.text = it.fat
+            binding.foodImage.downloadPics(it.picture, makePlaceHolder(requireContext()))
+        }
     }
 
     override fun onDestroyView() {

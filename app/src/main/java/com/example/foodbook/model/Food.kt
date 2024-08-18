4ba90d5af080d15b0d142ca226148248c8e2fe1c
ebruaeo <ebruaeo@hotmail.com>
2024-08-18 18:11:24 +0300

    string types changed to string?

--- app/src/main/java/com/example/foodbook/model/Food.kt
+++ app/src/main/java/com/example/foodbook/model/Food.kt
@@ -9,27 +9,27 @@ import com.google.gson.annotations.SerializedName
 data class Food(
     @ColumnInfo(name = "isim")
     @SerializedName("isim")
-    val name: String,
+    val name: String?,
 
     @ColumnInfo(name = "kalori")
     @SerializedName("kalori")
-    val calorie: String,
+    val calorie: String?,
 
     @ColumnInfo(name = "karbonhidrat")
     @SerializedName("karbonhidrat")
-    val carbs: String,
+    val carbs: String?,
 
     @ColumnInfo(name = "protein")
     @SerializedName("protein")
-    val protein: String,
+    val protein: String?,
 
     @ColumnInfo(name = "yag")
     @SerializedName("yag")
-    val fat: String,
+    val fat: String?,
 
     @ColumnInfo(name = "gorsel")
     @SerializedName("gorsel")
-    val picture: String
+    val picture: String?,
 ) {
     @PrimaryKey(autoGenerate = true)
     var uuid: Int = 0

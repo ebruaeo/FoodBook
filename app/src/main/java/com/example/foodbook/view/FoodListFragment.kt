e2ca97f869bd2559906dfa02fb928b5ad80d0dd3
ebruaeo <ebruaeo@hotmail.com>
2024-08-18 18:10:20 +0300

    Food list fragment viewmodel and adapter  initialized, and observing data

--- app/src/main/java/com/example/foodbook/view/FoodListFragment.kt
+++ app/src/main/java/com/example/foodbook/view/FoodListFragment.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.foodbook.adapter.FoodRecyclerAdapter
 import com.example.foodbook.databinding.FragmentFoodListBinding
 import com.example.foodbook.service.FoodAPI
+import com.example.foodbook.viewmodel.FoodListViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -19,6 +23,9 @@ class FoodListFragment : Fragment() {
 
     private var _binding: FragmentFoodListBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var viewModel: FoodListViewModel
+    private val foodRecyclerAdapter = FoodRecyclerAdapter(arrayListOf())
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,11 +45,48 @@ class FoodListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel = ViewModelProvider(this)[FoodListViewModel::class.java]
+        viewModel.refreshData()
+
+        binding.foodRecyclerView.layoutManager = LinearLayoutManager(context)
+        binding.foodRecyclerView.adapter = foodRecyclerAdapter
+
         binding.swipeRefreshLayout.setOnRefreshListener {
+            binding.foodRecyclerView.visibility = View.GONE
+            binding.errorMessage.visibility = View.GONE
+            binding.progressBar.visibility = View.VISIBLE
+            viewModel.refreshDataFromInternet()
+            binding.swipeRefreshLayout.isRefreshing = false
 
         }
 
+        observeLiveData()
+    }
 
+    private fun observeLiveData() {
+        viewModel.foods.observe(viewLifecycleOwner) {
+            binding.foodRecyclerView.visibility = View.VISIBLE
+            foodRecyclerAdapter.updateFoodList(it)
+        }
+
+        viewModel.foodErrorMessage.observe(viewLifecycleOwner) {
+            if (it) {
+                binding.errorMessage.visibility = View.VISIBLE
+                binding.foodRecyclerView.visibility = View.GONE
+            } else {
+                binding.errorMessage.visibility = View.GONE
+            }
+        }
+
+        viewModel.uploadingFood.observe(viewLifecycleOwner) {
+            if (it) {
+                binding.errorMessage.visibility = View.GONE
+                binding.foodRecyclerView.visibility = View.GONE
+                binding.progressBar.visibility = View.VISIBLE
+            } else {
+                binding.progressBar.visibility = View.GONE
+            }
+        }
     }
 
     override fun onDestroyView() {
